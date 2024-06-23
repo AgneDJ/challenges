c52bf2a226a41751a5ae8281b88c130090a007d5
AgneDJ <fizikee@gmail.com>
2024-06-23 00:12:22 -0700

    Challenge_13

--- challenge_13.py
+++ challenge_13.py
@@ -5,17 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
         # input [1,2,3]
-        # make them a one integer
+        # make them into one integer
         # increase by one
         # make them as array again
 
-        whole_number_str = str(digits)
-        whole_number_one_str = whole_number_str.replace(",", "")
+        whole_number_str = ''.join(map(str, digits))
+        whole_number_int = int(whole_number_str)
 
-        whole_number_int = int(whole_number_one_str)
-        whole_number_int_increased = whole_number_int + 1
-        whole_number_increased_str = str(whole_number_int_increased)
+        whole_number_int += 1
 
-        x = whole_number_increased_str.split(',')
+        whole_number_increased_str = str(whole_number_int)
+        result = [int(char) for char in whole_number_increased_str]
 
-        return increased_digits
+        return result  # output[1,2,4]
