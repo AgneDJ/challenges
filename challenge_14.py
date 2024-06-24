85da5bad36c3407f523c8daeec1164b0ce3e143d
AgneDJ <fizikee@gmail.com>
2024-06-24 13:28:32 -0700

    Challenge_14_buildin_comments

--- challenge_14.py
+++ challenge_14.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-
+    # build-in method
+        # sums up two inputs a and b(which are strings), by converting them into integers (int), a - number, 2 - declare it as binary, and ten makes sum into binary
         sum = bin(int(a, 2) + int(b, 2))
-        sum = sum[2:]
+        sum = sum[2:]  # removes the prefix 0b (binary prefix )
 
         return sum
