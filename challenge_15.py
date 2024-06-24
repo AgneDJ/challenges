38a337e3dcd29a393dc6fd3ba970b8287de7c229
AgneDJ <fizikee@gmail.com>
2024-06-24 14:13:24 -0700

    Challenge_15

---
+++ challenge_15.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        # input 4
+        # output 2
+        # in 2 out 1
+        # in 8 out 2
+
+        if x == 0:
+            return 0
+
+        a = x
+        b = (x + 1) / 2
+        while b < a:
+            a = b
+            b = (b + x / b) / 2
+
+        return a
