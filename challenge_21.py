7d8d61fc05a8e4e4c3490f25573c198513899c80
AgneDJ <fizikee@gmail.com>
2024-07-09 21:16:54 -0700

    Challenge_21_diff

--- challenge_21.py
+++ challenge_21.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution(object):
     def isSymmetric(self, root):
         """
@@ -24,3 +27,40 @@ class Solution(object):
             return (left.val == right.val) and check(left.left, right.right) and check(left.right, right.left)
 
         return check(root.left, root.right)
+
+
+# Definition for a binary tree node.
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        queue = deque([(root.left, root.right)])
+
+        while queue:
+            left, right = queue.popleft()
+
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+
+            # Enqueue next pairs
+            queue.append((left.left, right.right))
+            queue.append((left.right, right.left))
+
+        return True
