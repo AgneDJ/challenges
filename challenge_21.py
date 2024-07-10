57839f377ede38c6d0f0a413a38a12859fce887b
AgneDJ <fizikee@gmail.com>
2024-07-09 21:16:26 -0700

    Challenge_21

--- challenge_21.py
+++ challenge_21.py
@@ -1,1 +1,26 @@
-.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+
+        if not root:
+            return True
+
+        def check(left, right):
+            if not left and not right:
+                return True
+
+            if not left or not right:
+                return False
+
+            return (left.val == right.val) and check(left.left, right.right) and check(left.right, right.left)
+
+        return check(root.left, root.right)
