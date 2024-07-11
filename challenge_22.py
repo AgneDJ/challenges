bfcfeed68f2361346cf161b19f56cc60d6cd8e20
AgneDJ <fizikee@gmail.com>
2024-07-10 23:49:10 -0700

    Challenge_22

--- challenge_22.py
+++ challenge_22.py
@@ -1,1 +1,21 @@
-.
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        # Recursive case: compute the depth of left and right subtrees
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        # Return the maximum depth of the current subtree
+        return 1 + max(left_depth, right_depth)
